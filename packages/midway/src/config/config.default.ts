f80b2e2f7a80a714669fb21430f0f074c87a409e
yoshiyuki <brother-zhou@qq.com>
2022-07-17 15:13:57 +0800

    backend:config: fix hard-coded configs

--- packages/midway/src/config/config.default.ts
+++ packages/midway/src/config/config.default.ts
@@ -1,5 +1,5 @@
 import { MidwayConfig } from '@midwayjs/core';
-import * as path from "path";
+import * as path from 'path';
 
 export default {
   // use for cookie sign key, should change to your own and keep security
@@ -10,13 +10,13 @@ export default {
   //todo: for dev only, use proper DB for prod
   orm: {
     type: 'sqlite',
-    database: path.join('/Users/yoshiyuki/WebstormProjects/node-vue-login-demo/packages/midway/database.sqlite'),
+    database: path.join('../../database.sqlite'),
     synchronize: true,
     logging: true,
   },
   //todo: for dev only,use a .env file for prod
   jwt: {
     secret: 'xxxxxxxxxxxxxx', // fs.readFileSync('xxxxx.key')
-    expiresIn: '2d'
+    expiresIn: '2d',
   },
 } as MidwayConfig;
